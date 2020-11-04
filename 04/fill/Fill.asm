f8e5acfd852a80489248beb3f2ef0be7de6e5c4e
Emma <emma@virtualworthington.com>
2020-11-04 09:13:58 -0500

    idk

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -1,14 +1,14 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+(Loop)
+@KBD
+D=M
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
+@Skip
+D;JEQ
+D=-1
 
-// Put your code here.
+(Skip)
+@SCREEN
+M=D
+
+@Loop
+0;JMP
--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -1,9 +1,24 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
+@R2
+M=0
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+(loop)
+@R1
+D=M
+@end
+D;JEQ
 
-// Put your code here.
+@R0
+D=M
+
+@R2
+M=M+D
+
+@R1
+M=M-1
+
+@loop
+0; JMP
+
+(End)
+//@END
+//0;JMP
